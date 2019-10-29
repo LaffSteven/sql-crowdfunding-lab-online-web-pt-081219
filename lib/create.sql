984ff97ba354853cdaadae5a598fece1bf07e043
Steven Laff <steve.laff@gmail.com>
2019-10-29 00:04:11 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,5 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
+CREATE TABLE users (id INTEGER PRIMARY KEY, age INTEGER)
+
+CREATE TABLE pledges 
